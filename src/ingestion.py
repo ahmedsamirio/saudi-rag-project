993c9d29928c39a76c14c5e64371e08a1216bfdd
Ahmed Samir <ahmedsamirio95@gmail.com>
2024-04-27 19:24:02 +0300

    changed number reversing to apply to numbers only using regex

--- src/ingestion.py
+++ src/ingestion.py
@@ -9,13 +9,9 @@ import pyarabic.araby as araby
 def flip_arabic_line(line):
 
     def reverse_number(match):
-        # This part splits the number from its surrounding non-word characters
-        part = match.group(0)
-        # Isolate the number from commas and decimals for reversal
-        number = re.sub(r'[^\d]+', '', part)
-        reversed_number = number[::-1]
-        # Replace the original digits with the reversed digits
-        return re.sub(r'\d+', reversed_number, part, 1)
+        num = match.group(0)
+        # Reverse the entire number as a single unit, ignoring dots
+        return num[::-1]
 
     line = line[::-1]
     
@@ -24,7 +20,7 @@ def flip_arabic_line(line):
 
     for word in re.split(r'(\s+)', line):
 
-        if re.match(r'[^\w]*[+-]?[\d,]+(?:\.\d+)?[^\w]*', word):
+        if re.match(r'[0-9]+(\.[0-9]+)?', word):
             
             # Remove commas as it makes matching numbers and reversing them easier
             # This could be removed and the function that reverse numbers could 
@@ -32,7 +28,7 @@ def flip_arabic_line(line):
             number = word.replace(',', '')
 
             # Use regex to find all numbers and apply the reversing function
-            number = re.sub(r'[^\w]*[+-]?[\d,]+(?:\.\d+)?[^\w]*', reverse_number, number)
+            number = re.sub(r'[0-9]+(\.[0-9]+)?', reverse_number, number)
                 
             new_line.append(number)
 
